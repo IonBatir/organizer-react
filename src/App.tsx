dff01fdf9dce7832da99f6f2e9666ad8cfdb740e
Ion Batîr <ionbatir@gmail.com>
2021-06-12 20:53:31 +0300

    Set auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { useContext, useState, useEffect } from "react";
+import firebase from "firebase/app";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+import { Context } from ".";
+import { FIREBASE_AUTH_UI_CONFIG } from "./constants";
+
 export default function App() {
-  return <h1>Hello</h1>;
+  const { auth } = useContext(Context);
+  const [user, setUser] = useState<firebase.User | null>();
+
+  useEffect(() => auth.onAuthStateChanged(setUser), [auth]);
+
+  return (
+    <div>
+      {!user && (
+        <StyledFirebaseAuth
+          uiConfig={FIREBASE_AUTH_UI_CONFIG}
+          firebaseAuth={auth}
+        />
+      )}
+    </div>
+  );
 }
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+import firebase from "firebase/app";
+
+export const FIREBASE_AUTH_UI_CONFIG = {
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
+};

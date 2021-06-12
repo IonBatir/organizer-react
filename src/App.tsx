81a93daaae30eea7b2a7a0d14c38308eb806fee8
Ion Batîr <ionbatir@gmail.com>
2021-06-12 21:13:59 +0300

    Add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import { useContext, useState, useEffect } from "react";
 import firebase from "firebase/app";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import dayjs from "dayjs";
+import "dayjs/locale/ro";
 
+import "./App.css";
 import { Context } from ".";
 import { FIREBASE_AUTH_UI_CONFIG } from "./constants";
+import { Calendar, MonthSelector, Organizer } from "./components";
 
 export default function App() {
+  dayjs.locale("ro");
   const { auth } = useContext(Context);
   const [user, setUser] = useState<firebase.User | null>();
+  const [selectedDate, setSelectedDate] = useState(dayjs());
 
   useEffect(() => auth.onAuthStateChanged(setUser), [auth]);
 
   return user ? (
     <div className="container">
-      <header></header>
-      <main></main>
-      <div></div>
+      <header>
+        <MonthSelector
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+        />
+      </header>
+      <main>
+        <Calendar />
+      </main>
+      <div>
+        <Organizer />
+      </div>
     </div>
   ) : (
     <StyledFirebaseAuth

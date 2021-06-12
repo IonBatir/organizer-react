3441a750cf211dfac8a9840f427b8592c9225f38
Ion Batîr <ionbatir@gmail.com>
2021-06-12 20:45:22 +0300

    Config firebase

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBLP8WY6GNO8Fk_LB0a8oMIVb02GCHTvLI",
+  authDomain: "organizer-cc486.firebaseapp.com",
+  projectId: "organizer-cc486",
+  storageBucket: "organizer-cc486.appspot.com",
+  messagingSenderId: "185361287805",
+  appId: "1:185361287805:web:c8421b37cd652911483f14",
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { FIREBASE_CONFIG } from "./environment";
+
+firebase.initializeApp(FIREBASE_CONFIG);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export const Context = createContext({ auth, firestore });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={{ auth, firestore }}>
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

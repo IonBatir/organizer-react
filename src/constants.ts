8b7ab49ee9a31e24bebd6e25149d6149a81f7639
Ion Batîr <ionbatir@gmail.com>
2021-06-12 20:55:48 +0300

    Add email auth

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,10 @@ import firebase from "firebase/app";
 
 export const FIREBASE_AUTH_UI_CONFIG = {
   signInFlow: "popup",
-  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
   callbacks: {
     signInSuccessWithAuthResult: () => false,
   },

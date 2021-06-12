a2e8441c15cda5d284f70f06a7663a34dc8fec4e
Ion Batîr <ionbatir@gmail.com>
2021-06-12 21:13:47 +0300

    Set month selector component

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,3 +1,31 @@
-export default function MonthSelector() {
-  return <h1>MonthSelector</h1>;
+import dayjs from "dayjs";
+
+import "./MonthSelector.css";
+
+type Props = {
+  selectedDate: dayjs.Dayjs;
+  setSelectedDate: (date: dayjs.Dayjs) => void;
+};
+
+export default function MonthSelector({
+  selectedDate,
+  setSelectedDate,
+}: Props) {
+  const handleGoBackMonth = () =>
+    setSelectedDate(selectedDate.subtract(1, "month"));
+
+  const handleGoForwardMonth = () =>
+    setSelectedDate(selectedDate.add(1, "month"));
+
+  return (
+    <p>
+      <i className="material-icons" onClick={handleGoBackMonth}>
+        arrow_left
+      </i>
+      <span>{selectedDate.format("MMMM YYYY")}</span>
+      <i className="material-icons" onClick={handleGoForwardMonth}>
+        arrow_right
+      </i>
+    </p>
+  );
 }
